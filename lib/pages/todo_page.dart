66bf12de4fd88b84541337d62028126fd30bcfb7
abdellatif <mmakw57@gmail.com>
2026-02-14 02:56:37 +0200

    working version v0.3 with dismissible-tobedeleted

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -23,67 +23,127 @@ class _TodoPageState extends State<TodoPage> {
     int doneTodo = 0;
     int remTodo = 0;
     final primaryColor = Color.fromRGBO(79, 79, 79, 1);
+    final secondaryColor = Colors.grey[50];
     final prov = Provider.of<TodoProvider>(context);
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      // backgroundColor: Colors.red,
+      backgroundColor: secondaryColor,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15),
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
           child: Center(
             child: ConstrainedBox(
               constraints: BoxConstraints(maxWidth: 300),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Your To Do'),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: _input,
-                          decoration: InputDecoration(
-                            labelText: 'Task',
-                            hintText: 'Add new task',
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Your To Do'),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            cursorColor: primaryColor,
+                            onSubmitted: (value) {
+                              if (_input.text.isNotEmpty) {
+                                prov.addTodo(_input.text);
+                                _input.clear();
+                              }
+                            },
+                            controller: _input,
+                            decoration: InputDecoration(
+                              // labelText: 'Task',
+                              hintText: 'Add new task',
+
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: primaryColor,
+                                  width: 1.0,
+                                ),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: primaryColor,
+                                  width: 1.5,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          if (_input.text.isNotEmpty) {
-                            prov.addTodo(_input.text);
-                            _input.clear();
-                          }
-                        },
-                        icon: Icon(Icons.add),
-                      ),
-                    ],
-                  ),
-            
-                  /// Show a the list of todos
-                  Expanded(
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            side: const BorderSide(
-                              color: Colors.grey,
-                              width: 1
-                            )
+                        SizedBox(width: 10),
+                        IconButton(
+                          onPressed: () {
+                            if (_input.text.isNotEmpty) {
+                              prov.addTodo(_input.text);
+                              _input.clear();
+                            }
+                          },
+                          icon: Icon(Icons.add),
+                          style: IconButton.styleFrom(
+                            backgroundColor: primaryColor,
+                            foregroundColor: secondaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
-                          child: ListTile(
-                            title: Text(prov.todos[index].title),
-                            trailing: const Icon(Icons.arrow_forward_ios),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+
+                    /// Show
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return Dismissible(
+                          onDismissed: (direction) {
+                            if (direction == DismissDirection.startToEnd) {
+                              prov.removeTodo(index);
+                            }
+                          },
+                          background: Container(
+                            color: Colors.red,
+                            alignment: Alignment.centerLeft,
+                            padding: const EdgeInsets.only(left: 20.0),
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.white,
+                            ),
+                          ),
+                          direction: DismissDirection.startToEnd,
+                          key: UniqueKey(),
+                          child: Card(
+                            color: secondaryColor,
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 0,
+                              vertical: 8,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              side: BorderSide(color: primaryColor, width: 1),
+                            ),
+                            child: ListTile(
+                              leading: Checkbox(
+                                value: prov.todos[index].isDone,
+                                onChanged: (bool? newValue){
+                                  prov.toggleTodo(index);
+                                },
+                              ),
+                              title: Text(prov.todos[index].title),
+                              trailing: const Icon(Icons.arrow_forward_ios),
+                            ),
                           ),
                         );
                       },
                       itemCount: prov.todos.length,
                     ),
-                  ),
-            
-                  Text('Your remaining todos: ${prov.remainingTasks}')
-                ],
+                    SizedBox(height: 10),
+
+                    Text('Your remaining todos: ${prov.remainingTasks}'),
+                  ],
+                ),
               ),
             ),
           ),
